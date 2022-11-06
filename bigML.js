3580e69288bed5b25af6b77f959ab780dc2ec2a1
ol9999 <owenstuartlee@gmail.com>
2022-11-06 01:12:43 -0400

    working bigml score

--- bigML.js
+++ bigML.js
@@ -3,11 +3,13 @@ const deepnet = new bigml.LocalDeepnet(
      'deepnet/6367247c8be2aa364c004715', new bigml.BigML("owenstuartlee", "7815bbbc18255864b0df322cd83c8723b7a4f68b", {"domain": "bigml.io"}));
 module.exports = async function(inputText){
     return new Promise((resolve, reject) => {
-        deepnet.predict(inputData, (err, data)=>{
+        deepnet.predict({"Tweet": inputText}, (err, data)=>{
             if(err)
                 return reject(err);
-            console.log("Data: " + data);
-            resolve(data);
+            score=data["probability"]
+            if (data["prediction"]=='Democrat'){
+                score*=-1
+            } resolve(score);
         });
     });
 }
